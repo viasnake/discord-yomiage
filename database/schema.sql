2d5aaa641cdd29ef3655ae00c5abbf86d8629d13
snake <admin@viasnake.com>
2025-01-02 05:27:25 +0900

    update schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` INTEGER PRIMARY KEY,
-  `voice_languagecode` TEXT DEFAULT 'ja-JP',
-  `voice_name` TEXT DEFAULT 'ja-JP-Wavenet-C',
-  `audioconfig_speakingrate` REAL DEFAULT '1.0',
-  `audioconfig_pitch` REAL DEFAULT '0.0'
+  `voice_languagecode` TEXT DEFAULT 'ja-JP' NOT NULL,
+  `voice_name` TEXT DEFAULT 'ja-JP-Wavenet-C' NOT NULL,
+  `audioconfig_speakingrate` REAL DEFAULT '1.0' NOT NULL,
+  `audioconfig_pitch` REAL DEFAULT '0.0' NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `guilds` (
   `guild_id` INTEGER PRIMARY KEY,
-  `target_channel_id` INTEGER DEFAULT '0'
+  `target_channel_id` INTEGER DEFAULT '0' NOT NULL
 );
