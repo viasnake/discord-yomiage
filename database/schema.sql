4d3cf5f12002f8b56403d17244e9a1dc6ee0e7fd
snake <admin@viasnake.com>
2025-01-02 01:17:34 +0900

    fix typo

--- database/schema.sql
+++ database/schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_id` INTEGER PRIMARY KEY,
   `voice_languagecode` TEXT DEFAULT 'ja-JP',
   `voice_name` TEXT DEFAULT 'ja-JP-Wavenet-C',
-  `audioconfig_speakingrate` TEXT DEFAULT '1.0',
+  `audioconfig_speakingrate` TEXT DEFAULT '1.0'
 );
